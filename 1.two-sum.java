b592443a8af5f76ee859619b844e236cf46d989e
yashojhamvar <yasho170@gmail.com>
2018-03-08 23:05:49 -0500

    initial

---
+++ 1.two-sum.java
@@ -0,0 +1,41 @@
+/*
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (37.54%)
+ * Total Accepted:    828.6K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[3,2,4]\n6'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * 
+ * Example:
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=1;j<nums.length;j++){
+                if((nums[i]+nums[j])==target && i!=j){
+                     result = new int[] {i,j};
+                     return result;
+                }
+            }
+        }
+        return result;
+    }
+}
